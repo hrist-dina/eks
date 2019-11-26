d130a0122d26987cbad54e16621ed88f07566cc1
Dina Hrist <hdina@inbox.ru>
2019-11-27 01:21:00 +0700

    Added life search

--- src/blocks/components/recommendation/SwiperRecommended.js
+++ src/blocks/components/recommendation/SwiperRecommended.js
@@ -1,17 +1,3 @@
 import { SwiperBase } from "../../../js/classes/SwiperBase";
 
-export class SwiperRecommended extends SwiperBase {
-  bindOptions() {
-    this.screenWidht = 1840;
-    super.bindOptions({
-      breakpoints: {
-        1840: {
-          slidesPerView: 4
-        },
-        992: {
-          slidesPerView: "auto"
-        }
-      }
-    });
-  }
-}
+export class SwiperRecommended extends SwiperBase {}
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,73 +1,75 @@
 import $ from "jquery";
-import {MenuAccordion} from "%components%/menu-accordion/MenuAccordion";
-import {Select} from "%components%/select/Select";
-import {CatalogDetail} from "%components%/catalog-detail/CatalogDetail";
-import {FancyBox} from "../classes/FancyBox";
-import {SliderMain} from "%components%/slider-main/SliderMain";
-import {SliderVideo} from "%components%/slider-video/SliderVideo";
-import {SwiperRecommended} from "%components%/recommendation/SwiperRecommended";
-import {Map} from "%components%/map/Map";
-import {BaseModal} from "../classes/base-modal";
-import {Quantity} from "%components%/quantity/Quantity";
-import {WhereBuy} from "%components%/where-buy/WhereBuy";
-import {InputMask} from "../classes/InputMask";
-import {Form} from "../classes/Form";
-import {FormAuth} from "../classes/FormAuth";
-import {Sort} from "../classes/Sort";
-import {StoreBlock} from "../classes/StoreBlock";
-import {BuyModal} from "../classes/BuyModal";
-import {Basket} from "../classes/Basket";
-import {Preloader} from "../classes/Preloader";
-import {OrderForm} from "../classes/OrderForm";
-import {OrderProcessed} from "../classes/OrderProcessed";
-import {OrderHistoryItem} from "../classes/OrderHistoryItem";
+import { MenuAccordion } from "%components%/menu-accordion/MenuAccordion";
+import { Select } from "%components%/select/Select";
+import { CatalogDetail } from "%components%/catalog-detail/CatalogDetail";
+import { FancyBox } from "../classes/FancyBox";
+import { SliderMain } from "%components%/slider-main/SliderMain";
+import { SliderVideo } from "%components%/slider-video/SliderVideo";
+import { SwiperRecommended } from "%components%/recommendation/SwiperRecommended";
+import { Map } from "%components%/map/Map";
+import { BaseModal } from "../classes/base-modal";
+import { Quantity } from "%components%/quantity/Quantity";
+import { WhereBuy } from "%components%/where-buy/WhereBuy";
+import { InputMask } from "../classes/InputMask";
+import { Form } from "../classes/Form";
+import { FormAuth } from "../classes/FormAuth";
+import { Sort } from "../classes/Sort";
+import { StoreBlock } from "../classes/StoreBlock";
+import { BuyModal } from "../classes/BuyModal";
+import { Basket } from "../classes/Basket";
+import { Preloader } from "../classes/Preloader";
+import { OrderForm } from "../classes/OrderForm";
+import { OrderProcessed } from "../classes/OrderProcessed";
+import { OrderHistoryItem } from "../classes/OrderHistoryItem";
+import { Search } from "../../blocks/components/search/Search";
 
-$(document).ready(function () {
-    new SliderMain(".js-slider-main");
-    new SliderVideo(".js-slider-video");
-    new MenuAccordion();
-    new Select();
-    new CatalogDetail();
-    new FancyBox();
-    new SwiperRecommended(".js-swiper-recommendation");
-    new BaseModal();
-    new Quantity();
-    new InputMask().phone();
-    new FormAuth(".js-form-auth");
-    new Map("map-barnaul", {
-        location: [53.32387907112886, 83.64198349999995],
-        center: [53.32387907112886, 83.64198349999995],
-        zoom: 16,
-        hint: "ул. Попова , 242"
-    });
-    new Map("map-moscow", {
-        location: [55.883661068828225, 37.51370899999999],
-        center: [55.883661068828225, 37.51370899999999],
-        zoom: 17,
-        hint: "Коровинское шоссе 35 стр.2"
-    });
-    new WhereBuy();
-    new Form();
-    new Sort();
-    $('.js-stores-wrap').each((i, el) => {
-        new StoreBlock(el);
-    });
-    $('.js-modal-add2basket').each((i, el) => {
-        new BuyModal(el);
-    });
-    $('.js-basket').each((i, el) => {
-        new Basket(el);
-    });
-    $('.js-wrapper').each((i, el) => {
-        new Preloader(el);
-    });
-    $('.js-order-processed').each((i, el) => {
-        new OrderProcessed(el);
-    });
-    $('.js-form-order').each((i, el) => {
-        new OrderForm(el);
-    });
-    $('.js-order-history-item').each((i, el) => {
-        new OrderHistoryItem(el);
-    });
+$(document).ready(function() {
+  new SliderMain(".js-slider-main");
+  new SliderVideo(".js-slider-video");
+  new MenuAccordion();
+  new Select();
+  new CatalogDetail();
+  new FancyBox();
+  new SwiperRecommended(".js-swiper-recommendation");
+  new BaseModal();
+  new Quantity();
+  new InputMask().phone();
+  new FormAuth(".js-form-auth");
+  new Map("map-barnaul", {
+    location: [53.32387907112886, 83.64198349999995],
+    center: [53.32387907112886, 83.64198349999995],
+    zoom: 16,
+    hint: "ул. Попова , 242"
+  });
+  new Map("map-moscow", {
+    location: [55.883661068828225, 37.51370899999999],
+    center: [55.883661068828225, 37.51370899999999],
+    zoom: 17,
+    hint: "Коровинское шоссе 35 стр.2"
+  });
+  new WhereBuy();
+  new Form();
+  new Sort();
+  $(".js-stores-wrap").each((i, el) => {
+    new StoreBlock(el);
+  });
+  $(".js-modal-add2basket").each((i, el) => {
+    new BuyModal(el);
+  });
+  $(".js-basket").each((i, el) => {
+    new Basket(el);
+  });
+  $(".js-wrapper").each((i, el) => {
+    new Preloader(el);
+  });
+  $(".js-order-processed").each((i, el) => {
+    new OrderProcessed(el);
+  });
+  $(".js-form-order").each((i, el) => {
+    new OrderForm(el);
+  });
+  $(".js-order-history-item").each((i, el) => {
+    new OrderHistoryItem(el);
+  });
+  new Search();
 });

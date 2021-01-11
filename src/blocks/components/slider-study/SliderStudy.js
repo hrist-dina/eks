319bb247caeb58f44a420685b9f3e0e72b59cc06
root <root@test.letsrock.pro>
2021-01-11 11:43:18 +0700

    *bugfix:sliderStudy slider bugfix

--- src/blocks/components/slider-study/SliderStudy.js
+++ src/blocks/components/slider-study/SliderStudy.js
@@ -3,29 +3,51 @@ import SwiperBase from "../../../js/classes/SwiperBase";
 import Swiper from "swiper";
 
 class SliderStudy {
-    constructor(selector, options) {
+    constructor(selector) {
         this.selector = selector;
-        this.options = options;
+        this.activeSlide =
+        // this.options = options;
 
         this.init();
     }
     init() {
         // super.init();
         // this.bindOptions();
-        console.log(this.options);
         this.initSwiper();
+        this.swiperAfterInit();
     }
 
     initSwiper() {
         if ($(this.selector).length) {
             new Swiper(this.selector, {
                 direction: 'vertical',
-                slidesPerView: 1,
-                spaceBetween: 30,
+                slidesPerView: 5,
+                // loopedSlides: 3,
+                // loopAdditionalSlides: 3,
                 mousewheel: true,
+                atuoHeight: true,
                 loop: true,});
         }
     }
+
+    swiperAfterInit() {
+        this.wrapper = $(this.selector).find('.js-study__slider');
+        this.activeItem = this.wrapper.find('.js-study-sidebar__item.active');
+        this.centerActiveItem();
+    }
+
+    centerActiveItem() {
+        if (!this.wrapper || !this.activeItem) return false;
+
+        let activeItemOffsetTop = this.activeItem.offset().top;
+        let wrapperHeight = this.wrapper.height();
+        console.log(wrapperHeight);
+        let centerPosition = wrapperHeight / 2;
+        console.log(centerPosition);
+        let activeItemCenterOffset  = Number(centerPosition) - Number(activeItemOffsetTop);
+        console.log(activeItemCenterOffset);
+    }
+
 
     bindOptions(options) {
         let defaultOptions = {

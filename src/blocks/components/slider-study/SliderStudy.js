3990866513d44717a81946830ad2af58559d3b82
Dina Hrist <hdina@inbox.ru>
2021-02-10 00:57:39 +0700

    * Try fix slider study

--- src/blocks/components/slider-study/SliderStudy.js
+++ src/blocks/components/slider-study/SliderStudy.js
@@ -4,15 +4,17 @@ import Swiper from "swiper";
 class SliderStudy {
     constructor(selector, options = {}) {
         this.selector = selector;
-        if (options.preloadSelector) this.preloadContainer = $(options.preloadSelector);
+        if (options.preloadSelector)
+            this.preloadContainer = $(options.preloadSelector);
         this.loopedSlides = 6;
         this.nextSlideAddIndexesCount = 1;
-        this.slideDuplicateSelector = '.swiper-slide-duplicate';
-        this.disabledClass = 'disabled';
-        this.linkSelector = '.study-sidebar__link';
+        this.slideDuplicateSelector = ".swiper-slide-duplicate";
+        this.disabledClass = "disabled";
+        this.linkSelector = ".study-sidebar__link";
         this.prevSlide = null;
         this.prevSlideIndex = null;
         this.nextSlide = null;
+        this.isInit = null;
         this.init();
     }
     init() {
@@ -23,31 +25,39 @@ class SliderStudy {
         if ($(this.selector).length) {
             this.swiper = new Swiper(this.selector, {
                 init: false,
-                direction: 'vertical',
-                slidesPerView: 'auto',
+                direction: "vertical",
+                slidesPerView: "auto",
                 loopedSlides: this.loopedSlides,
                 mousewheel: {
                     releaseOnEdges: true,
+                    sensitivity: .4,
+                    thresholdTime: .7
                 },
                 loop: true,
                 height: 450,
-                speed: 200,
+                speed: 400,
                 centeredSlides: true,
                 freeMode: true,
+                breakpoints: {
+                    1024: {
+                        height: 500,
+                    },
+                }
             });
 
             let self = this;
 
-            this.swiper.on('init', self.removePreload.bind(self));
-            this.swiper.on('init', self.checkIfSlidingIsAvailable.bind(self));
-
+            this.swiper.on("init", self.removePreload.bind(self));
+            //this.swiper.on('init', self.checkIfSlidingIsAvailable.bind(self));
 
             if (this.swiper) {
-                this.swiper.on('slideChange', () => {
-                    this.swiper.$wrapperEl.trigger('scroll');
-                });
+                // this.swiper.on("slideChange", () => {
+                //     this.swiper.$wrapperEl.trigger('scroll');
+                // });
 
-                this.swiper.on('slideChange', this.checkIfSlidingIsAvailable.bind(this));
+                this.swiper.on("slideChange", this.processChangeSlide.bind(this));
+
+                // this.swiper.on('slideChange', this.checkIfSlidingIsAvailable.bind(this));
             }
 
             this.swiper.init();
@@ -55,10 +65,55 @@ class SliderStudy {
     }
 
     removePreload() {
-        this.preloadContainer.removeClass('preloader-visible');
-        this.swiper.$el.css('opacity', '1');
-        this.preloadContainer.find('.preloader-loader').remove();
-        this.preloadContainer.find('.preloader-wrap').remove();
+        this.preloadContainer.removeClass("preloader-visible");
+        this.swiper.$el.css("opacity", "1");
+        this.preloadContainer.find(".preloader-loader").remove();
+        this.preloadContainer.find(".preloader-wrap").remove();
+        this.isInit = true;
+    }
+
+    processChangeSlide() {
+        if(!this.isInit) return false;
+
+        const activeIndex = this.swiper.activeIndex;
+        const prevIndex = activeIndex - 1;
+        const nextIndex = activeIndex + 1;
+        const active = $(this.swiper.slides[activeIndex]);
+
+        const prev = $(this.swiper.slides[prevIndex]);
+        const next = $(this.swiper.slides[nextIndex]);
+
+        const isActiveDisabledLink = active
+            .find(this.linkSelector)
+            .hasClass(this.disabledClass);
+
+        if (
+            isActiveDisabledLink &&
+            !prev.find(this.linkSelector).hasClass(this.disabledClass)
+        ) {
+            this.swiper.slideTo(prevIndex);
+        } else if (
+            isActiveDisabledLink &&
+            !next.find(this.linkSelector).hasClass(this.disabledClass)
+        ) {
+            this.swiper.slideTo(nextIndex);
+        } else if (!isActiveDisabledLink) {
+            $(this.swiper.slides).removeClass("active");
+            active.addClass("active");
+            const activeLink = active.find(this.linkSelector);
+            window.location = activeLink.attr("href");
+        } else {
+            let activeFindIndex = null;
+            this.swiper.slides.each((i, el) => {
+                if ($(el).hasClass('active')) {
+                    activeFindIndex = i;
+                    return false;
+                }
+            });
+            if (activeFindIndex) {
+                this.swiper.slideTo(activeFindIndex);
+            }
+        }
     }
 
     checkIfSlidingIsAvailable() {
@@ -66,11 +121,17 @@ class SliderStudy {
         console.log(this.prevSlideIndex);
         let activeIndex = this.swiper.activeIndex;
         // Отнимаем количество loopedSlides, т.к. в бесконечном слайдере это количество прибавляется к РЕАЛЬНОМУ индексу слайда
-        let prevSlideIndex = activeIndex - this.loopedSlides - this.nextSlideAddIndexesCount;
-        let nextSlideIndex = activeIndex - this.loopedSlides + this.nextSlideAddIndexesCount;
+        let prevSlideIndex =
+      activeIndex - this.loopedSlides - this.nextSlideAddIndexesCount;
+        let nextSlideIndex =
+      activeIndex - this.loopedSlides + this.nextSlideAddIndexesCount;
 
-        this.prevSlide = this.swiper.$el.find(`.swiper-slide[data-swiper-slide-index='${prevSlideIndex}']:not(${this.slideDuplicateSelector})`);
-        this.nextSlide = this.swiper.$el.find(`.swiper-slide[data-swiper-slide-index='${nextSlideIndex}']:not(${this.slideDuplicateSelector})`);
+        this.prevSlide = this.swiper.$el.find(
+            `.swiper-slide[data-swiper-slide-index='${prevSlideIndex}']:not(${this.slideDuplicateSelector})`
+        );
+        this.nextSlide = this.swiper.$el.find(
+            `.swiper-slide[data-swiper-slide-index='${nextSlideIndex}']:not(${this.slideDuplicateSelector})`
+        );
 
         this.checkIfPrevSlideIsEnabled();
         this.checkIfNextSlideIsEnabled();
